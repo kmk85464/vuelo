b3dfcb94fe4c6fa1458434568ec49ccb027e08d5
Roberto Calvo <KMK85464@educastur.es>
2024-12-23 13:11:52 +0100

    FixFecha

--- vuelo/src/App.js
+++ vuelo/src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React, { useState } from 'react';
+import vuelosData from './vuelos.json'; // Ajusta la ruta según la ubicación del archivo
 
 function App() {
+  const [flights] = useState(vuelosData); // Inicializa el estado con los datos del JSON
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Listado de Vuelos</h1>
       </header>
+      <main>
+        {flights.map((destination) => (
+          <Destino key={destination.destination} destination={destination} />
+        ))}
+      </main>
+    </div>
+  );
+}
+
+function Destino({ destination }) {
+  return (
+    <div className="destino">
+      <h2>Destino: {destination.destination}</h2>
+      <ul>
+        {destination.flights.map((flights) => (
+          <li key={flights.id}>
+            <strong>Vuelo:</strong> {flights.id} - <strong>Fecha:</strong> {flights.date}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
