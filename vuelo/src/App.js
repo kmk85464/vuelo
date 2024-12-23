b206906140bb5fdfd6160ff3899bfdcea47d2046
Roberto Calvo <KMK85464@educastur.es>
2024-12-23 17:22:47 +0100

    FixVuelo

--- vuelo/src/App.js
+++ vuelo/src/App.js
@@ -2,9 +2,14 @@
 import React, { useState } from 'react';
 import vuelosData from './vuelos.json'; // Ajusta la ruta según la ubicación del archivo
 
+function onSeleccionarVuelo(vuelo) {
+  console.log('Vuelo seleccionado:', vuelo);
+}
+
 function App() {
   const [flights] = useState(vuelosData); // Inicializa el estado con los datos del JSON
-
+  const [vueloSeleccionado, setVueloSeleccionado] = useState(null); // Estado para el vuelo seleccionado
+  const [vuelos] = useState(vuelosData || []); // Carga los datos iniciales
   return (
     <div className="App">
       <header>
@@ -12,8 +17,14 @@ function App() {
       </header>
       <main>
         {flights.map((destination) => (
-          <Destino key={destination.destination} destination={destination} />
+          <Destino key={destination.destination} destination={destination} 
+          onSeleccionarVuelo={(vuelo) => setVueloSeleccionado(vuelo)}
+          />
         ))}
+         {/* Panel de detalles del vuelo seleccionado */}
+         {vueloSeleccionado && (
+          <PanelDetalleVuelo vuelo={vueloSeleccionado} />
+        )}
       </main>
     </div>
   );
@@ -27,6 +38,13 @@ function Destino({ destination }) {
         {destination.flights.map((flights) => (
           <li key={flights.id}>
             <strong>Vuelo:</strong> {flights.number} - <strong>Fecha:</strong> {flights.date}
+            <button
+              onClick={() =>
+                onSeleccionarVuelo({ ...flights, destino: destination.destination })
+              }
+            >
+              Seleccionar
+            </button>
           </li>
         ))}
       </ul>
@@ -34,4 +52,17 @@ function Destino({ destination }) {
   );
 }
 
+function PanelDetalleVuelo({ flights }) {
+  return (
+    <div className="panel-detalle">
+      <h3>Detalle del Vuelo</h3>
+      <p><strong>Destino:</strong> {Destino.destination}</p>
+      <p><strong>Fecha:</strong> {flights.date}</p>
+      <p><strong>Hora:</strong> {flights.time}</p>
+      <p><strong>Plazas Totales:</strong> {flights.plazasDisponibles + (flights.plazasOcupadas || 0)}</p>
+      <p><strong>Plazas Disponibles:</strong> {flights.plazasDisponibles}</p>
+      <p><strong>Plazas Ocupadas:</strong> {flights.plazasOcupadas || 0}</p>
+    </div>
+  );
+}
 export default App;
