78a6dfde090a3599bfc630ecabf6e34f1fa82b7b
Roberto Calvo <KMK85464@educastur.es>
2025-01-02 15:06:28 +0100

    Fix dependencia

--- vuelo/src/reportWebVitals.js
+++ vuelo/src/reportWebVitals.js
@@ -1,4 +1,4 @@
-/*
+
 const reportWebVitals = onPerfEntry => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
@@ -12,4 +12,3 @@ const reportWebVitals = onPerfEntry => {
 };
 
 export default reportWebVitals;
-*/
